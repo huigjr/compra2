45dc0242cf17ade228c773ed8ac3369c16beec1f
huigjr <huigjr@gmail.com>
2023-01-30 18:59:20 +0100

    Admin design

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_HOST', 'localhost'  ); // Server Location
-define('DB_NAME', 'roadrunner' ); // Database Name
+define('DB_NAME', 'compra'     ); // Database Name
 define('DB_USER', 'root'       ); // Username
 define('DB_PASS', ''           ); // Password
 define('ROOT', dirname(__DIR__)); // Base Filepath
