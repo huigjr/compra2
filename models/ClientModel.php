a4aed0bd3d780183033b90399b87e09b92022668
huigjr <huigjr@gmail.com>
2023-02-02 12:21:32 +0100

    Added to clients

--- models/ClientModel.php
+++ models/ClientModel.php
@@ -4,4 +4,10 @@ class ClientModel extends BaseModel
 {
     protected $id = 'clientid';
     protected $table = 'clients';
+
+    public function read($value, $column = null)
+    {
+        $this->page->partial('list', "SELECT * FROM `products`");
+        parent::read($value);
+    }
 }
